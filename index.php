516a63deb720ceeead26b763ac0b2cd1677882e4
artur <a.boykinya@gmail.com>
2020-10-12 13:13:07 +0300

    without links

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+error_reporting(E_ALL);
+$nameList[0] = "Yuen Wo Ping";
+$nameList[1] = "Geofrey Darrow";
+$nameList[2] = "Hugo Weaving";
+for($i = 0; $i < cunt($nameList); $i++){
+    echo "This is {$i} element of "
+}
 
 

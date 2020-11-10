b28496b09af2724b1637fc828a7ed225e6e7fd76
artur <a.boykinya@gmail.com>
2020-11-10 19:49:20 +0200

    SQL

--- homework/2020.10.26/author.php
+++ homework/2020.10.26/author.php
@@ -9,8 +9,28 @@ if (!$login || !$password) {
 }
 
 $config = require __DIR__ . '/config.php';
+$link = mysqli_connect(
+    $config['db']['host'],
+    $config['db']['user'],
+    $config['db']['password'],
+    $config['db']['db'],
+);
 
-$hash = $config['users'][$login] ?? null;
+if (!$link) {
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+$sql = "SELECT * FROM users WHERE login = ?";
+$stmt = mysqli_prepare($link, $sql);  // підготовлює SQL  запрос
+mysqli_stmt_bind_param($stmt, 's',$login); // Привязка переменных к параметрам подготавливаемого запроса
+mysqli_stmt_execute($stmt); // Выполняет подготовленный запрос
+$result = mysqli_stmt_get_result($stmt);  // Получает результат из подготовленного запроса
+$user = mysqli_fetch_assoc($result);     // функція яка перетворює нашу строку з бази даеих в масив.
+
+$hash = $user['password'] ?? null;
 
 if( !$hash || !password_verify($password, $hash)){
     exit('Login and password are required');
--- homework/2020.10.26/config.php
+++ homework/2020.10.26/config.php
@@ -4,9 +4,10 @@ return [
     'baseDir' => __DIR__.'/customer',
     'webRout' => '/homework/2020.10.26/customer',
     'web' => '/homework/2020.10.26',
-    'users' => [
-        'user' => '$2y$10$k.MkUcX5jxiUGPDCc3cmPet5K5jO07YYAjLltOm5WCjwGsVRk6jUe',
-        'user2' => '$2y$10$ttW2agg9o7RJLUL41COjG.PTkvUrbY/AB/4M8/U6P2K1CMyJhzGam',
-        'user3' => '$2y$10$QqQ49S1VVeC/Pwg1jvxvI.V1S/liRzH8oKilDyxDOeYwg7dzrbz7O',
-]
+    'db' =>[
+        'host' => 'db',
+        'user' => 'artur_user',
+        'password' => 'artur_pwd',
+        'db' => 'artur_db',
+    ]
 ];

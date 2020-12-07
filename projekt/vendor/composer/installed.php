a99fec036b062a3c05c605ee409248d076e253b6
artur <a.boykinya@gmail.com>
2020-12-07 17:56:27 +0200

    +project

---
+++ projekt/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b2f2dd5f52006af79929baef394d2436b5c03735',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b2f2dd5f52006af79929baef394d2436b5c03735',
+    ),
+  ),
+);
